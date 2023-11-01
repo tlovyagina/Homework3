cde7fba7fc8bc888063a397536f805645ac737b7
tlovyagina <tatiana.loviagina@yandex.ru>
2023-11-01 21:14:50 +0300

    Final commit

---
+++ src/main/java/org/example/MultiplicationTablePrinter.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MultiplicationTablePrinter {
+    public void print (){
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++)
+                System.out.print(i + "*" + j + "=" + i * j + "\t");
+            System.out.println();
+        }
+    }
+}
